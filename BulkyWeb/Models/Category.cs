9686743e9f3a37096e5263af86777f7781cfc3b6
Amna Hodžić <ahodzic649@outlook.com>
2023-10-28 11:24:42 +0200

    DbConnection

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+		[Key] //not required , .netf will automatically assume Id is the primary key
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+
+		
+	}
+}
