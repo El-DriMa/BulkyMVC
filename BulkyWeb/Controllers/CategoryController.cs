ac2a743980e3b161c1938a989bab13de3fcd14b6
Amna Hodžić <ahodzic649@outlook.com>
2023-11-03 13:48:41 +0100

    Edit and Delete Category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -4,22 +4,71 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-	public class CategoryController : Controller
-	{
-		private readonly ApplicationDbContext _db;
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
         public CategoryController(ApplicationDbContext db)
         {
-			_db = db;
+            _db = db;
         }
         public IActionResult Index()
-		{
-			List<Category> objCategoryList = _db.Categories.ToList();
-			return View(objCategoryList);
-		}
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
 
-		public IActionResult Create()
-		{
+        public IActionResult Create()
+        {
             return View();
-		}
-	}
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display odrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+                return NotFound();
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display odrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+
+    }
 }
